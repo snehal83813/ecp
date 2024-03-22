1b6489c2da4c3df481848a864ee56963c832f87d
Snehal212Mahajan <mahajansnehal227@gmail.com>
2024-03-22 17:02:06 +0530

    solutions

---
+++ Assignment-6/ass6_7.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<string.h>
+int value(char r)
+{
+	if(r=='I')
+		return 1;
+	if(r=='V')
+		return 5;
+	if(r=='L')
+		return 50;
+	if(r=='C')
+		return 500;
+	if(r=='M')
+		return 1000;
+	return -1;
+}
+int romanToDecimal(char str[])
+{
+int res=0;
+
+for(int i=0;i<(strlen(str));i++)
+{
+	int s1=value(str[i]);
+
+	if(i+1<strlen(str))
+	{
+		int s2=value(str[i+1]);
+
+		if(s1>=s2)
+		{
+			res=res+s1;
+		}
+		else
+		{
+			res= res + s2 - s1;
+			i++;
+		}
+	}
+	else
+	{
+		res=res+s1;
+	}
+}
+return res;
+}
+int main()
+{
+	char str[10]="MCMIV";
+	printf("integer form of roman numeral is %d",romanToDecimal(str));
+	return 0;
+}
